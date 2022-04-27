c25b4af5e28ed89766c8352a655b707fb349bdb5
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-04-27 21:54:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const productsRoutes = require("./routes/products");
+
+const PORT = 3001;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("./api/products/products", productsRoutes);
+
+app.listen(PORT);
---
+++ routes/products.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  const pathJSON = path.resolve(__dirname, "../api/products/products.json");
+  fs.readFile(pathJSON, (err, data) => {
+    res.json(JSON.parse(data));
+  });
+});
+
+
+module.exports = router;
